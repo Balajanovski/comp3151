9c7bffc8dfda1b9298d4db1e6e9e98531d03f4b3
Balajanovski <jbalajanbusiness@gmail.com>
2023-07-04 00:20:37 +1000

    - Cleaned up solution

--- ass1/java/SortedArray.java
+++ ass1/java/SortedArray.java
@@ -140,86 +140,11 @@ public class SortedArray {
     public void delete(int x) {
         assert (x > 0);
 
-        int low = 0;
-        int high = N+1;
-
-        // Locking left and right of binary search prevents
-        // swaps travelling over a binary search region
-        // invalidating it
-        this.swap_locks[low].lock();
-        this.swap_locks[high].lock();
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            // Lock to ensure that the value to potentially be deleted doesn't change
-            this.swap_locks[mid].lock();
-            int mid_value = this.values[mid];
-
-            if (mid_value == -1) {
-                // Search right
-                boolean found_mid = false;
-                int currently_locked = mid;
-                for (int i = mid+1; i <= high; ++i) {
-                    this.swap_locks[i].lock();
-                    this.swap_locks[currently_locked].unlock();
-                    currently_locked = i;
-
-                    mid_value = this.values[i];
-
-                    if (mid_value > 0) {
-                        mid = i;
-                        found_mid = true;
-                        break;
-                    } else if (mid_value == 0) {
-                        // We have found the boundary of the array
-                        // Early exit
-                        break;
-                    }
-                }
-
-                // Search left if not found a "midpoint"
-                if (!found_mid) {
-                    for (int i = mid-1; i >= low; --i) {
-                        this.swap_locks[i].lock();
-                        this.swap_locks[currently_locked].unlock();
-                        currently_locked = i;
-
-                        mid_value = this.values[i];
-
-                        if (mid_value > 0) {
-                            mid = i;
-                            found_mid = true;
-                            break;
-                        }
-                    }
-                }
-
-                if (!found_mid) {
-                    this.swap_locks[currently_locked].unlock();
-                    break;
-                }
-            }
-
-            if (mid_value == 0 || mid_value > x) {
-                this.swap_locks[mid-1].lock();
-                this.swap_locks[high].unlock();
-                this.swap_locks[mid].unlock();
-                high = mid-1;
-            } else if (mid_value < x) {
-                this.swap_locks[mid+1].lock();
-                this.swap_locks[low].unlock();
-                this.swap_locks[mid].unlock();
-                low = mid+1;
-            } else {
-                this.values[mid] = -1;
-                this.swap_locks[mid].unlock();
-                break;
-            }
+        int x_index = find(x);
+        if (x_index != -1) {
+            this.values[x_index] = -1;
+            this.swap_locks[x_index].unlock();
         }
-
-        this.swap_locks[low].unlock();
-        this.swap_locks[high].unlock();
 
         this.size_semaphore.release();
         this.size.addAndGet(-1);
@@ -229,13 +154,50 @@ public class SortedArray {
         // -1 encodes a deleted member
         // 0 encodes array padding
 
+        int x_index = find(x);
+        if (x_index != -1) {
+            this.swap_locks[x_index].unlock();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void print_sorted() {
+        this.swap_locks[0].lock();
+        for (int i = 1; i < N+2; ++i) {
+            this.swap_locks[i].lock();
+            if (values[i] == Integer.MAX_VALUE) {
+                this.swap_locks[i-1].unlock();
+                this.swap_locks[i].unlock();
+                return;
+            }
+
+            if (values[i] > 0) {
+                System.out.println(values[i]);
+            }
+
+            this.swap_locks[i-1].unlock();
+        }
+        this.swap_locks[N+1].unlock();
+    }
+
+    public void cleanup() {
+
+    }
+
+    // =-=-=-=-=-=-=-=-
+    // Helper functions
+    // =-=-=-=-=-=-=-=-
+
+    private int find(int x) {
         int low = 0;
         int high = N+1;
-        boolean found_member = false;
+        int x_index = -1;
 
         // Locking left and right of binary search prevents
         // swaps travelling over a binary search region
-        // invalidating it
+        // and potentially invalidating it
         this.swap_locks[low].lock();
         this.swap_locks[high].lock();
 
@@ -302,12 +264,7 @@ public class SortedArray {
                 this.swap_locks[mid].unlock();
                 low = mid+1;
             } else {
-                this.swap_locks[mid].unlock();
-
-                // Possible interleaving where element gets deleted here
-                // This is ok according to the assignment spec
-
-                found_member = true;
+                x_index = mid;
                 break;
             }
         }
@@ -315,29 +272,6 @@ public class SortedArray {
         this.swap_locks[low].unlock();
         this.swap_locks[high].unlock();
 
-        return found_member;
-    }
-
-    public void print_sorted() {
-        this.swap_locks[0].lock();
-        for (int i = 1; i < N+2; ++i) {
-            this.swap_locks[i].lock();
-            if (values[i] == Integer.MAX_VALUE) {
-                this.swap_locks[i-1].unlock();
-                this.swap_locks[i].unlock();
-                return;
-            }
-
-            if (values[i] > 0) {
-                System.out.println(values[i]);
-            }
-
-            this.swap_locks[i-1].unlock();
-        }
-        this.swap_locks[N+1].unlock();
-    }
-
-    public void cleanup() {
-
+        return x_index;
     }
 }
